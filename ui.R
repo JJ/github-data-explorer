2c8b74c0da2fb5bc39e227a068ed8eb3213a1210
JJ Merelo <jjmerelo@gmail.com>
2015-02-16 18:38:39 +0100

    First shiny App thanks to http://shiny.rstudio.com/articles/shinyapps.html

---
+++ ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(ggplot2)
+
+dataset <- read.csv("top-alt-Spain.csv",sep=';')
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("GitHub ranking Explorer"),
+  
+  sidebarPanel(
+    
+    selectInput('x', 'X', names(dataset)),
+    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+    selectInput('color', 'Color', c('None', names(dataset))),
+    
+    checkboxInput('jitter', 'Jitter'),
+    checkboxInput('smooth', 'Smooth'),
+    
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+  ),
+  
+  mainPanel(
+    plotOutput('plot')
+  )
+))
+
